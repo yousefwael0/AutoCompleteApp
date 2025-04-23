186152aa4942a98d104007545291e654911af3b1
NourKhalil05 <nour21hani@gmail.com>
2025-04-23 19:47:19 +0200

    NewWordTracker

--- src/new_word_tracker.cpp
+++ src/new_word_tracker.cpp
@@ -1,6 +1,29 @@
 #include "new_word_tracker.h"
+#include <algorithm>
+#include <iostream>
 
-void NewWordTracker::recordNewWord(const std::string& word, Trie& trie, FrequencyManager& freqManager) {
+using namespace std;
+
+
+void NewWordTracker::recordNewWord(const string& word, Trie& trie, FrequencyManager& freqManager) {
     // Track word frequency
     // If word typed 3 times, insert into Trie and FrequencyManager
+
+    if (word.empty()) { return; }
+
+    string lowerWord;
+    lowerWord.reserve(word.size());
+    transform(word.begin(), word.end(), back_inserter(lowerWord), tolower);
+
+    if (trie.searchExact(lowerWord)) { return; }
+
+    int currentCount = ++newWordsCount[lowerWord];
+
+    if (currentCount == INSERT_THRESHOLD) {
+        trie.insert(lowerWord);
+        freqManager.addNewWord(lowerWord);
+        freqManager.increaseFrequency(lowerWord);
+        newWordsCount.erase(lowerWord);
+    }
 }
+
