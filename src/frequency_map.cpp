b0fe08a7f7ae13043458de024e4bc76f40605c81
Yousef <yusufatta9@gmail.com>
2025-04-23 19:14:13 +0200

    Revert "wrote implementation for frequency map"
    This reverts commit 6b3c2759badf1ef592ed2e598c2b893c24b729da.

--- src/frequency_map.cpp
+++ src/frequency_map.cpp
@@ -1,56 +1,21 @@
-#include "FrequencyManager.h"
-#include <algorithm>
-#include <iostream>
-
-using namespace std;
+#include "frequency_map.h"
 
 FrequencyManager::FrequencyManager() {
-    // Constructor - nothing to initialize manually
+    // Constructor
 }
 
-void FrequencyManager::addNewWord(const string& word) {
-    // Already confirmed? Do nothing
-    if (frequencyMap.find(word) != frequencyMap.end()) {
-        cout << " Word already added";
-        return;
-    }
-    // If in pending, increment counter
-    if (pendingMap.find(word) != pendingMap.end()) {
-        pendingMap[word]++;
-
-        // Promote to confirmed if it hits 3
-        if (pendingMap[word] >= 3) {
-            frequencyMap[word] = 0;
-            pendingMap.erase(word);
-        }
-    }
-    else {
-        // Brand new word, add to pending with 1 count
-        pendingMap[word] = 1;
-    }
+void FrequencyManager::addNewWord(const std::string& word) {
+    // Add new word with frequency 0
 }
 
-void FrequencyManager::increaseFrequency(const string& word) {
-    // Only increase if the word has been confirmed
-    if (frequencyMap.find(word) != frequencyMap.end()) {
-        frequencyMap[word]++;
-    }
+void FrequencyManager::increaseFrequency(const std::string& word) {
+    // Increase frequency by 1
 }
 
-int FrequencyManager::getFrequency(const string& word) const {
-    auto it = frequencyMap.find(word);
-    if (it != frequencyMap.end()) {
-        return it->second;
-    }
-    return 0;
+int FrequencyManager::getFrequency(const std::string& word) const {
+    // Return frequency
 }
 
-vector<string> FrequencyManager::sortByFrequency(const vector<string>& words) const {
-    vector<string> sortedWords = words;
-
-    sort(sortedWords.begin(), sortedWords.end(), [this](const string& a, const string& b) {
-        return getFrequency(a) > getFrequency(b); // Descending
-        });
-
-    return sortedWords;
+std::vector<std::string> FrequencyManager::sortByFrequency(const std::vector<std::string>& words) const {
+    // Sort given words by their frequency
 }
--- src/frequency_map.h
+++ src/frequency_map.h
@@ -3,18 +3,14 @@
 #include <vector>
 #include <string>
 
-using namespace std;
-
 class FrequencyManager {
 private:
-    unordered_map<string, int> frequencyMap;  // Confirmed words + frequency
-    unordered_map<string, int> pendingMap;    // New words pending approval
+    std::unordered_map<std::string, int> frequencyMap;
 
 public:
     FrequencyManager();
-
-    void addNewWord(const string& word);                      // Adds new or updates pending
-    void increaseFrequency(const string& word);               // Only increases if in confirmed map
-    int getFrequency(const string& word) const;               // Returns 0 if not in confirmed
-    vector<string> sortByFrequency(const vector<string>& words) const;  // Sort words by freq
+    void addNewWord(const std::string& word);
+    void increaseFrequency(const std::string& word);
+    int getFrequency(const std::string& word) const;
+    std::vector<std::string> sortByFrequency(const std::vector<std::string>& words) const;
 };
