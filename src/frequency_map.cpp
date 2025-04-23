4e19e210d1657665b1c6bec634020fd7904ba1b4
Yousef <yusufatta9@gmail.com>
2025-04-23 19:15:12 +0200

    Reapply "wrote implementation for frequency map"
    This reverts commit b0fe08a7f7ae13043458de024e4bc76f40605c81.

--- src/frequency_map.cpp
+++ src/frequency_map.cpp
@@ -1,21 +1,56 @@
-#include "frequency_map.h"
+#include "FrequencyManager.h"
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
 
 FrequencyManager::FrequencyManager() {
-    // Constructor
+    // Constructor - nothing to initialize manually
 }
 
-void FrequencyManager::addNewWord(const std::string& word) {
-    // Add new word with frequency 0
+void FrequencyManager::addNewWord(const string& word) {
+    // Already confirmed? Do nothing
+    if (frequencyMap.find(word) != frequencyMap.end()) {
+        cout << " Word already added";
+        return;
+    }
+    // If in pending, increment counter
+    if (pendingMap.find(word) != pendingMap.end()) {
+        pendingMap[word]++;
+
+        // Promote to confirmed if it hits 3
+        if (pendingMap[word] >= 3) {
+            frequencyMap[word] = 0;
+            pendingMap.erase(word);
+        }
+    }
+    else {
+        // Brand new word, add to pending with 1 count
+        pendingMap[word] = 1;
+    }
 }
 
-void FrequencyManager::increaseFrequency(const std::string& word) {
-    // Increase frequency by 1
+void FrequencyManager::increaseFrequency(const string& word) {
+    // Only increase if the word has been confirmed
+    if (frequencyMap.find(word) != frequencyMap.end()) {
+        frequencyMap[word]++;
+    }
 }
 
-int FrequencyManager::getFrequency(const std::string& word) const {
-    // Return frequency
+int FrequencyManager::getFrequency(const string& word) const {
+    auto it = frequencyMap.find(word);
+    if (it != frequencyMap.end()) {
+        return it->second;
+    }
+    return 0;
 }
 
-std::vector<std::string> FrequencyManager::sortByFrequency(const std::vector<std::string>& words) const {
-    // Sort given words by their frequency
+vector<string> FrequencyManager::sortByFrequency(const vector<string>& words) const {
+    vector<string> sortedWords = words;
+
+    sort(sortedWords.begin(), sortedWords.end(), [this](const string& a, const string& b) {
+        return getFrequency(a) > getFrequency(b); // Descending
+        });
+
+    return sortedWords;
 }
