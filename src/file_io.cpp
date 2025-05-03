d7f85bdd7454066c064b205a7671e47135adad85
Yousef <yusufatta9@gmail.com>
2025-05-03 22:02:14 +0300

    Fixed file handling

--- src/file_io.cpp
+++ src/file_io.cpp
@@ -1,16 +1,8 @@
 #include "file_io.h"
 #include <fstream>
 #include <sstream>
-#iclude <algorithm>
-#iclude <cctype>  //for toLower()
 
 using namespace std;
-
-// Convert string to lowercase using <algorithm> (transform())
-string toLower(const string& str) {
-    string lowered = str;
-    transform(lowered.begin(), lowered.end(), lowered.begin(), ::tolower);
-    return lowered;
 
 bool FileManager::loadData(const string& filename, Trie& trie, FrequencyManager& freqManager) {// Open file and load into Trie and FrequencyManager
     
@@ -29,13 +21,9 @@ bool FileManager::loadData(const string& filename, Trie& trie, FrequencyManager&
             continue; // skip malformed lines
         }
 
-        word = toLower(word); // ensure case insensitivity
-
         trie.insert(word);
 
-        for (int i = 0; i < frequency; ++i) {
-            freqManager.increaseFrequency(word);
-        }
+        freqManager.setFrequency(word, frequency);
     }
 
     infile.close();
@@ -56,65 +44,4 @@ void FileManager::saveData(const string& filename, const FrequencyManager& freqM
     }
 
     outfile.close();
-
-}
-#include "file_io.h"
-#include <fstream>
-#include <sstream>
-#iclude <algorithm>
-#iclude <cctype>  //for toLower()
-
-using namespace std;
-
-// Convert string to lowercase using <algorithm> (transform())
-string toLower(const string& str) {
-    string lowered = str;
-    transform(lowered.begin(), lowered.end(), lowered.begin(), ::tolower);
-    return lowered;
-
-bool FileManager::loadData(const string& filename, Trie& trie, FrequencyManager& freqManager) {// Open file and load into Trie and FrequencyManager
-    
-    ifstream infile(filename);
-    if (!infile.is_open()) {
-        return false;
-    }
-
-    string line;
-    while (getline(infile, line)) {
-        istringstream iss(line);
-        string word;
-        int frequency;
-
-        if (!(iss >> word >> frequency)) {
-            continue; // skip malformed lines
-        }
-
-        word = toLower(word); // ensure case insensitivity
-
-        trie.insert(word);
-
-        for (int i = 0; i < frequency; ++i) {
-            freqManager.increaseFrequency(word);
-        }
-    }
-
-    infile.close();
-    return true;
-}
-
-
-void FileManager::saveData(const string& filename, const FrequencyManager& freqManager) {// Save frequency map back to file
-    
-    ofstream outfile(filename);
-    if (!outfile.is_open()) {
-        return;
-    }
-
-    const auto& freqMap = freqManager.getMap();  //add getMap() method to freq class
-    for (const auto& pair : freqMap) {
-        outfile << pair.first << " " << pair.second << endl;
-    }
-
-    outfile.close();
-
 }
